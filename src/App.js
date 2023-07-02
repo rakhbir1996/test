48d786f178d709054d3c271154dc4af098028270
rakhbir1996 <rakhbirsingh1996@gmail.com>
2023-07-02 16:10:07 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import {FunctionComponent, FunctionComponent2} from './FunctionComponent.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h2>I am App Component</h2>
+        <FunctionComponent />
+        <FunctionComponent2 />
+      </header> 
+    </div>
+     
+  );
+}
+
+export default App;
---
+++ src/FunctionComponent.js
@@ -0,0 +1,13 @@
+export function FunctionComponent() {
+  const name = "jass";
+
+  return <h2>I am User Component {name}</h2>;
+}
+
+export function FunctionComponent2() {
+  return <h2>I am User "Component"</h2>;
+  
+}
+
+
+// export default User;
